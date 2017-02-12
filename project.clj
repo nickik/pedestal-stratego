9f2515c863de017afbc6e80c781a2003fc5ec431
Niklaus Zbinden <niklaus@contovista.ch>
2017-02-12 09:52:33 +0100

    Version update

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [io.pedestal/pedestal.service "0.3.1"]
 
                  [io.pedestal/pedestal.jetty "0.3.1"]
